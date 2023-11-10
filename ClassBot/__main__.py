6dd09dfcc0ceeb6d5bbe537f3ee147fe98bb20ba
SergiPantoja <62522545+SergiPantoja@users.noreply.github.com>
2023-11-10 05:15:15 -0800

    Setting up stuff for bot

--- ClassBot/__main__.py
+++ ClassBot/__main__.py
@@ -23,3 +23,21 @@ from models.base import Base
 
 Base.metadata.create_all(engine)
 logger.info("Created database tables.")
+
+
+# ptb imports
+from telegram.ext import (
+    Application,
+)
+
+
+
+# bot
+TOKEN = "5827425180:AAE6HGte6-L50z8IWysZ1jVng02zc1qxDaw"
+
+app = Application.builder().token(TOKEN).read_timeout(30).write_timeout(30).build()
+
+
+
+# start the bot (ctrl-c to stop)
+app.run_polling()
