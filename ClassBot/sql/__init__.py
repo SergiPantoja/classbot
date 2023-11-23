559ad150a502cd88424bdf9a8c12f87ca273fac7
SergiPantoja <62522545+SergiPantoja@users.noreply.github.com>
2023-11-22 22:18:00 -0800

    Create token types at database creation

--- ClassBot/sql/__init__.py
+++ ClassBot/sql/__init__.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import sessionmaker, Session
 
 from utils.logger import logger
 from models.base import Base
+from models.token_type import Token_type
 
 
 def _start():
@@ -18,8 +19,19 @@ def _start():
 
     return Session
 
+def create_default_token_types(session):
+    # at the start of the bot, create the default token types usable by the bot
+    # in every course.
+    with session() as s:
+        # create default token types
+        s.add(Token_type(type="Medalla"))
+        s.add(Token_type(type="Misceláneo"))
+        s.commit()
+
+
 try:
     session = _start()
+    create_default_token_types(session)
 except Exception as e:
     logger.exception(f"failed to connect due to {e}")
     raise e
---
+++ ClassBot/sql/token_type_sql.py
@@ -0,0 +1,22 @@
+from sqlalchemy import select
+
+from models.token_type import Token_type
+from sql import session
+
+
+def get_token_type(id: int) -> Token_type | None:
+    """ Returns a token_type object with the given id. None if not found."""
+    with session() as s:
+        return s.query(Token_type).filter(Token_type.id == id).first()
+    
+def get_token_type_by_type(type: str) -> Token_type | None:
+    """ Returns the first token_type object with the given type. None if not found."""
+    with session() as s:
+        return s.query(Token_type).filter(Token_type.type == type).first()
+
+
+def add_token_type(type: str, course_id: int = None, hidden: bool = False) -> None:
+    """ Adds a new token_type to the database. """
+    with session() as s:
+        s.add(Token_type(type=type, course_id=course_id, hidden=hidden))
+        s.commit()
